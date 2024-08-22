572425c7ab2a5ec6dbc868920bc6045f9e96b564
Z Cater <zcater42@gmail.com>
2024-08-22 17:08:29 +0000

    Refactored TileContents into just two tileset-specific functions

--- Tile.hs
+++ Tile.hs
@@ -5,17 +5,22 @@ import Data.Function (on)
 
 type Tile a = Either (Set.Set a) a
 data Connections a = Connections {north :: a, south :: a, east :: a, west :: a} deriving (Functor, Foldable, Traversable)
+instance Applicative Connections where
+    pure x = Connections x x x x
+    (Connections f0 f1 f2 f3) <*> (Connections x0 x1 x2 x3) = Connections (f0 x0) (f1 x1) (f2 x2) (f3 x3)
 
 class TileContent a where
     pretty :: Tile a -> String
-    valid :: Connections (Maybe (Tile a)) -> a -> Bool
+    validators :: Connections (a -> a -> Bool)
 
 defaultTile :: Enum a => Tile a
 defaultTile = Left $ Set.fromDistinctAscList [toEnum 0 ..]
 
 collapse :: TileContent a => Connections (Maybe (Tile a)) -> Tile a -> Tile a
 collapse neighbors (Left set) = packTile $ simplify neighbors set
-  where simplify = Set.filter . valid
+  where simplify = Set.filter . validate
+        validate c x = foldr1 (&&) $ check <$> (validators <*> pure x) <*> c
+        check valid = maybe True (either (any valid) valid)
         packTile = fmap headF . guardedE ((==1) . Set.size)
         headF = foldr const (error "head: empty foldable structure")
         guardedE p x = if p x then pure x else Left x
@@ -27,7 +32,7 @@ data Pipes = NS | WE | NW | SW | NE | SE | XX | OO deriving (Eq, Enum)
 
 instance TileContent Pipes where
 
-    --pretty :: Tile -> String
+    --pretty :: Tile Pipes -> String
     pretty = pure . either (const ' ') showPipes
         where showPipes = \case
                 NS -> '║'
@@ -39,15 +44,9 @@ instance TileContent Pipes where
                 XX -> '╬'
                 OO -> ' '
 
-    --valid :: Connections (Maybe (Tile Pipes)) -> Pipes -> Bool
-    valid c x = (check matchE $ east c) && (check matchW $ west c) && (check matchN $ north c) && (check matchS $ south c)
+    -- validators :: Connections (Pipes -> Pipes -> Bool)
+    validators = flip on getConnections <$> match
       where
-        check :: (Connections Bool -> Connections Bool -> Bool) -> Maybe (Tile Pipes) -> Bool
-        check f = \case
-            Nothing -> True
-            Just (Right y) -> (f `on` getConnections) x y
-            Just (Left y) -> any ((f `on` getConnections) x) y
-
         getConnections :: Pipes -> Connections Bool
         getConnections = \case
             NS -> Connections True True False False 
@@ -59,9 +58,10 @@ instance TileContent Pipes where
             XX -> Connections True True True True
             OO -> Connections False False False False
 
-        matchE, matchW, matchN, matchS :: Connections Bool -> Connections Bool -> Bool
-        matchE a b = east a == west b
-        matchW a b = west a == east b
-        matchN a b = north a == south b
-        matchS a b = south a == north b
-
+        match :: Connections (Connections Bool -> Connections Bool -> Bool)
+        match = Connections {east = matchE, west = matchW, south = matchS, north = matchN}
+          where
+            matchE a b = east a == west b
+            matchW a b = west a == east b
+            matchN a b = north a == south b
+            matchS a b = south a == north b
